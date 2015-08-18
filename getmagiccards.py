9f7d69c1a1820308f9b2c45d77fe98882a876658
7thGate <geoffgbasore@gmail.com>
2015-08-17 23:53:09 -0400

    Updated getmagiccards.py
    Submitting an updated version of getmagiccards.py that actually works,
    since the right lines are uncommented.  It will need tweaking before
    being run if you don't have E: as your root drive, since the paths are
    hard-coded.  Added comments.

--- getmagiccards.py
+++ getmagiccards.py
@@ -14,12 +14,16 @@ import urllib
 import re
 import os
 
+#Function for downloading one card based on the set prefix+card number.  It constructs the appropriate URL to hit magiccards.info's .jpg for that card
+#and saves it to a hard-coded local directory structure based on the code for the set.
 def downloadCard(setname, cardnumber, cardname):
     if(not os.path.exists("E:\\Magic Card Images\\" + setname + "\\")):
         os.mkdir("E:\\Magic Card Images\\" + setname + "\\")
     urllib.urlretrieve("http://magiccards.info/scans/en/"+setname+"/"+cardnumber+".jpg", "E:\\Magic Card Images\\" + setname + "\\" + cardnumber + ".jpg")
     return
 
+#DownloadSet takes a set prefix and scrapes the english version of the directory listing for that set on magiccards.info.
+#It enumerates the cards in the set, and calls downloadCard on each in turn to pull the card images to the local hard drive.
 def downloadSet(setname):
     print(setname)
     setdata = urllib.urlopen("http://magiccards.info/"+setname+"/en.html")
@@ -37,6 +41,7 @@ def downloadSet(setname):
 dir = urllib.urlopen("http://magiccards.info/sitemap.html")
 data = dir.read()
 toFind = re.escape("<small style=\"color: #aaa;\">") + "(\w*)" + re.escape("</small>")
+#Use a regex to scrape the set prefixes out of the sitemap.
 it = re.finditer(toFind, str(data))
 while True:
     try:
@@ -44,7 +49,10 @@ while True:
     except StopIteration:
         break
     setname = value.group(1)
+    #de is the indicator that we're over into german cards.  Stop at that point, I don't think there is much to gain by having all the foreign language versions at this point.
     if setname == "de":
-        break
-#    if setname != "en":
-#        downloadSet(setname)
+         break
+    #Skip en, it is the indicator that we're in the language section (the languages get flagged as "sets" too by the regex)
+    #Otherwise, go ahead and grab the set
+    if setname != "en":
+        downloadSet(setname)
