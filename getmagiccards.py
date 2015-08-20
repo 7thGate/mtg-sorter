6032e848e933bb32c39d00629d19a63e2e3f65c7
awesor <peter.a.minassian@gmail.com>
2015-08-19 23:22:34 -0400

    adding a python 3 version
    converted using 2to3 and also added an if block for checking the os
    version sysname.  Darwin = mac

--- getmagiccards.py
+++ getmagiccards.py
@@ -10,27 +10,33 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import re
 import os
 
 #Function for downloading one card based on the set prefix+card number.  It constructs the appropriate URL to hit magiccards.info's .jpg for that card
 #and saves it to a hard-coded local directory structure based on the code for the set.
 def downloadCard(setname, cardnumber, cardname):
-    if(not os.path.exists("E:\\Magic Card Images\\" + setname + "\\")):
-        os.mkdir("E:\\Magic Card Images\\" + setname + "\\")
-    urllib.urlretrieve("http://magiccards.info/scans/en/"+setname+"/"+cardnumber+".jpg", "E:\\Magic Card Images\\" + setname + "\\" + cardnumber + ".jpg")
+    #Operating System check, mac is Darwin...pc is...else!
+    if(os.uname().sysname == "Darwin"):
+        if(not os.path.exists("/Users/peter/Documents/Projects/MTG/Images/" + setname + "/")):
+            os.mkdir("/Users/peter/Documents/Projects/MTG/Images/" + setname + "/")
+    else:
+        if(not os.path.exists("E:\\Magic Card Images\\" + setname + "\\")):
+            os.mkdir("E:\\Magic Card Images\\" + setname + "\\")
+    
+    urllib.request.urlretrieve("http://magiccards.info/scans/en/"+setname+"/"+cardnumber+".jpg", "/Users/peter/Documents/Projects/MTG/Images/" + setname + "/" + cardnumber + ".jpg")
     return
 
 #DownloadSet takes a set prefix and scrapes the english version of the directory listing for that set on magiccards.info.
 #It enumerates the cards in the set, and calls downloadCard on each in turn to pull the card images to the local hard drive.
 def downloadSet(setname):
     print(setname)
-    setdata = urllib.urlopen("http://magiccards.info/"+setname+"/en.html")
+    setdata = urllib.request.urlopen("http://magiccards.info/"+setname+"/en.html")
     it = re.finditer(re.escape("<a href=\"/") + setname + re.escape("/en/") + "(\d*\w*)" + re.escape(".html\">") + "([^<]*)", str(setdata.read()))
     while True:
         try:
-            value = it.next()
+            value = next(it)
         except StopIteration:
             break
         cardnumber = value.group(1)
@@ -38,7 +44,7 @@ def downloadSet(setname):
         downloadCard(setname, cardnumber, cardname)
     return
 
-dir = urllib.urlopen("http://magiccards.info/sitemap.html")
+dir = urllib.request.urlopen("http://magiccards.info/sitemap.html")
 data = dir.read()
 toFind = re.escape("<small style=\"color: #aaa;\">") + "(\w*)" + re.escape("</small>")
 #Use a regex to scrape the set prefixes out of the sitemap.
